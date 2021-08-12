5b9267ee7a592d744128bd3243651c08df6ccd22
chrishoran8 <horan_chris@hotmail.co.uk>
2021-08-12 12:17:53 +0100

    started

--- PythonEssentialsModule2/PythonEssentialsModule2.py
+++ PythonEssentialsModule2/PythonEssentialsModule2.py
@@ -1,1 +1,28 @@
 
+#task 2.1.1.18 solution
+print("Programming","Essentials","in", sep="***", end="...")
+
+#2.1.1.19 original
+
+print("    *")
+print("   * *")
+print("  *   *")
+print(" *     *")
+print("***   ***")
+print("  *   *")
+print("  *   *")
+print("  *****")
+
+#bullet 1
+print("     *\n", "   * *\n", "  *   *\n", " *     *\n", "***   ***\n", "  *   *\n", "  *   * \n", "  *****\n")
+#or
+print("    *\n   * *\n  *   *\n *     *\n***   ***\n  *   *\n  *   *\n  *****")
+
+print("    *     "*2)
+print("   * *    "*2)
+print("  *   *   "*2)
+print(" *     *  "*2)
+print("***   *** "*2)
+print("  *   *   "*2)
+print("  *   *   "*2)
+print("  *****   "*2)
