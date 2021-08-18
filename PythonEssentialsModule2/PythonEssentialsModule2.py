b1e3c2ae48470a89c768711c920ce030fb59e8c8
chrishoran8 <horan_chris@hotmail.co.uk>
2021-08-18 10:01:30 +0100

    km/miles lab

--- PythonEssentialsModule2/PythonEssentialsModule2.py
+++ PythonEssentialsModule2/PythonEssentialsModule2.py
@@ -28,6 +28,9 @@ print("  *   *   "*2)
 print("  *****   "*2)
 
 #operators
+
+#apples lab
+
 #bullet 1 and 2
 john = 3
 mary = 5
@@ -45,3 +48,15 @@ print(total_apples)
 #average
 appleAverage = (john + mary + adam) // 3
 print(appleAverage)
+
+#lab on converting km to miles
+
+kilometers = 12.25 #normal assignment of 12.25 to new km variable
+miles = 7.38 #normal assigment of 7.38 to new miles variable
+
+miles_to_kilometers = miles/0.62 # evaluate expression of miles/0.62 and assign 
+kilometers_to_miles = kilometers/1.61
+
+print(miles, "miles is", round(miles_to_kilometers, 2), "kilometers") # print statement
+print(kilometers, "kilometers is", round(kilometers_to_miles, 2), "miles")
+
